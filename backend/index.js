742e2d192658fba642ee557497e0eb339fe3994e
rkurniawati <rkurniawati@users.noreply.github.com>
2022-12-02 02:48:54 +0000

    Empty shell working

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
+import express from 'express';
+
 const app = express()
-const port = 3000
+const port = 8000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
