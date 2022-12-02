5e1c11b054fc995e072a105b9c531874dd69d62d
Ruth Kurniawati <rkurniawati@westfield.ma.edu>
2022-12-02 03:14:37 +0000

    mongodb backend running

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,100 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient, ObjectID } from 'mongodb';
 
-const app = express()
-const port = 8000
+const MONGO_URL = 'mongodb://localhost:27017';
+const MONGO_DATABASE = "dogs";
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+let dbClient = null;
+const connect = async (url) => {
+    let client = await MongoClient.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });    
+    return client;
+}
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+const getConnection = async () => {
+    if (!dbClient) {
+        dbClient = await connect(MONGO_URL);
+        if (!dbClient) {
+            console.log("Failed to connect to mongodb");
+            process.exit(1);
+        }
+    }
+    return dbClient.db(MONGO_DATABASE);
+}
+
+const getAllDogs = async () => {
+    const database = await getConnection();
+    const values = await database.collection("dogs").find({}).toArray();
+    return values;
+}
+
+const deleteDog = async (tableName, id) => {
+    const database = await getConnection();
+    await database.collection("dogs").deleteOne({_id: ObjectID(id)});    
+}
+
+const addDog = async (tableName, name, age, breed) => {
+    const database = await getConnection();
+    const dogRecord = {
+        "name" : name,
+        "age" : age,
+        "breed" : breed,
+    }
+    await database.collection("dogs").insertOne(dogRecord);    
+}
+
+const routes = [
+    {
+        method: 'get',
+        path: '/hello',
+        handler: async (req, res) => {
+            res.send("Hello!");
+        },
+    },
+    {
+        method: 'get',
+        path: '/dogs',
+        handler: async (req, res) => {
+            const values = await getAllDogs();
+            res.status(200).json(values);
+        },
+    },
+    {
+        method: 'post',
+        path: '/adddog',
+        handler: async (req, res) => {
+            const { collection, name, age, breed } = req.body;
+            await addDog(collection, name, age, breed);
+            res.status(200).json({ status: "ok"});
+        },
+    },
+    {
+        method: 'post',
+        path: '/deletedog',
+        handler: async (req, res) => {
+            const { collection, id } = req.body;
+            await deleteDog(collection, id);
+            res.status(200).json({ status: "ok"});
+        },
+    },
+];
+
+const BACKEND_PORT = 8000;
+const app  = express();
+app.use(bodyParser.json());
+
+routes.forEach(route => {
+    app[route.method](route.path, route.handler);
+});
+
+const start = async () => {
+    await connect(MONGO_URL);
+    app.listen(BACKEND_PORT, () => {
+        console.log(`Server is up at port ${BACKEND_PORT}`)
+    })
+    
+}
+start();
